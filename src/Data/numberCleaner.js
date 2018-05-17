1c313267764ba1b40f47ae6fc9985bcdf0cef218
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-16 22:28:29 -0600

    Add billion option for numberCleaner to account for the game's most expensive items

--- src/Data/numberCleaner.js
+++ src/Data/numberCleaner.js
@@ -2,7 +2,9 @@ const numberCleaner = (number) => {
   if (number === 0) {
     return number
   }
-  if (number.includes('m')) {
+  if (number.includes('b')) {
+    return number.split('').filter(letter => letter !== 'b').join('') * 1000000000
+  } else if (number.includes('m')) {
     return number.split('').filter(letter => letter !== 'm').join('') * 1000000
   } else if (number.includes('k')) {
     return number.split('').filter(letter => letter !== 'k').join('') * 1000
